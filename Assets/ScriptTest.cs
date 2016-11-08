a8daad870201574d3298561b392f841758b49519
Bastien Pichot <bastien.pichot@avanade.com>
2016-11-08 10:51:41 +0100

    first test commit

---
+++ Assets/ScriptTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptTest : MonoBehaviour {
+
+    [SerializeField]
+    public int Counter = 0;
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
